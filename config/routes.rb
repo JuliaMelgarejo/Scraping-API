2ff39765133979e45cc294f69f5a69670da1b8ee
root <root@Mati>
2024-10-31 17:59:32 -0300

    adding email

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   get 'users', to: 'admin#index', as: :users_panel
-  devise_for :users
+  devise_for :users, controllers: { invitations: 'devise/invitations' }
   resources :links
   resources :subscriptions
   resources :notifications
